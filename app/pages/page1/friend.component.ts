497733effb97e107b87c413b982ccc8ffa1cfd22
thainh107 <thainh10791@gmail.com>
2016-10-04 09:13:45 +0700

    Load event when change leave type

--- app/pages/page1/friend.component.ts
+++ app/pages/page1/friend.component.ts
@@ -6,32 +6,30 @@ import {MyData} from '../../providers/my-data/my-data';
 @Component({
     selector: 'myfriends',
     template: `
-        <ion-row *ngFor="let dev of devList;let i = index" style="width:980px" class="rowChild">
-                <div style="width:16.67%; margin-left:0%" class="infor textInfo">
+        <ion-row *ngFor="let dev of devList;let i = index" style="width:980px"  class="rowChild">
+                <div style="width:16.67%; margin-left:0%" class="textInfo item-info" >
                     {{dev.name}} - {{dev.phoneno}}
-                </div>
-                <div [ngStyle]="{width:(dev.start-1)/42*100+'%'}"></div>
-                <div class="{{dev.kind}}" [ngStyle]="{width:dev.days/42*100+'%'}">
-                
+                </div>                
+                <div id="r_{{i}}" class=" item-leave {{dev.kind}}" [ngStyle]="{width:dev.days/42*100+'%','margin-left':(dev.start - 1) / 42 * 100+'%'}">
                     {{dev.start}} - {{dev.end}} {{shortMonth}} ({{dev.location}})
                     </div>
 
             </ion-row>
     `,
-  providers:[MyData]
+    providers: [MyData]
 })
-export class FriendComponent{
-    //plan 2 :<div id="r_{{i}}" class="{{dev.kind}}" [ngStyle]="{width:dev.days/42*100+'%','margin-left':(dev.start - 1) / 42 * 100+'%'}">
-    devList:any;
-    componentName:any;
-    tempM:any;
-    constructor(public events:Events,myData : MyData){     
-        var d=new Date();
+export class FriendComponent {
+    //plan 2 :<div [ngStyle]="{width:(dev.start-1)/42*100+'%'}"></div><div class="item-leave {{dev.kind}}" [ngStyle]="{width:dev.days/42*100+'%'}">
+    devList: any;
+    componentName: any;
+    tempM: any;
+    constructor(public events: Events, myData: MyData) {
+        var d = new Date();
         this.tempM = d.getMonth();
-        this.events.subscribe('myEvent',(object) => {
-            this.devList=object[0];
+        this.events.subscribe('myEvent', (object) => {
+            this.devList = object[0];
         });
-        
+
     }
 
 }
--- app/pages/page1/page1.ts
+++ app/pages/page1/page1.ts
@@ -8,7 +8,7 @@ import {MyData} from '../../providers/my-data/my-data';
 @Component({
   templateUrl: 'build/pages/page1/page1.html',
   directives: [FriendComponent],
-  providers:[MyData]
+  providers: [MyData]
 })
 
 export class Page1 {
@@ -26,8 +26,17 @@ export class Page1 {
   endW5day: String;
   endW5month: String;
   endM: number;
+  str: any;
+  filterWork: boolean;
+  filterTrip: boolean;
+  filterOff: boolean;
 
-  constructor(public navCtrl: NavController, public events: Events,myData : MyData) {
+  constructor(public navCtrl: NavController, public events: Events, myData: MyData) {
+
+    this.filterWork = true;
+    this.filterTrip = true;
+    this.filterOff = true;
+
     var d = new Date();
     this.month = ["January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December"];
@@ -42,10 +51,11 @@ export class Page1 {
     this.leavetype = '';
     //load data and excute function
     myData.load()
-    .then(user => this.items = user)
-    .then(user=>this.changeMonth());
+      .then(user => this.items = user)
+      .then(user => this.changeMonth());
   }
   changeMonth() {
+    this.str = new Array;
     let displayDate = new Date(this.year);
     this.devList = this.items.filter(item => item.month.startsWith(displayDate.getMonth()));
 
@@ -90,6 +100,101 @@ export class Page1 {
     return monthNames[m].toString();
   }
 
+
+  // filterType(type: number) {
+  //       let displayDate = new Date(this.year);
+  //       // if(type===1 && this.str.indexOf("work")<=-1){
+  //       //   this.str.push("work");
+  //       // document.getElementById(type.toString()).setAttribute("class", "btn-disabled");
+  //       // }
+  //       // if(type===1 && this.str.indexOf("work")>=0){
+  //       //   // this.str.delete("work");
+  //       //   document.getElementById(type.toString()).setAttribute("class", "disable-hover work");
+  //       // }
+  //       // if (type===2 && this.str.indexOf("off")<=-1){
+  //       //   this.str.push("off");
+  //       //   document.getElementById(type.toString()).setAttribute("class", "btn-disabled");
+  //       // }
+  //       // if (type===3 && this.str.indexOf("trip")<=-1){
+  //       //   this.str.push("trip");
+  //       //   document.getElementById(type.toString()).setAttribute("class", "btn-disabled");
+  //       // }
+  //       // console.log(this.str);
+  //   if (type === 1) {
+  //     if (document.getElementById(type.toString()).className.includes("btn-disabled")) {
+  //       document.getElementById(type.toString()).setAttribute("class", "disable-hover work");
+  //       this.devList = this.items.filter(item => item.month.startsWith(displayDate.getMonth()));
+  //       this.events.publish('myEvent', this.devList);
+  //       console.log(this.devList);
+  //     }
+  //     else {
+  //       document.getElementById(type.toString()).setAttribute("class", "btn-disabled");
+  //       this.devList = this.devList.filter(item => !item.kind.startsWith('work'));
+  //       this.events.publish('myEvent', this.devList);
+  //       console.log(this.devList);
+  //     }
+  //   }
+  //   else if (type === 2 && !(document.getElementById("1").className.includes("btn-disabled")) && (document.getElementById(type.toString()).className.includes("btn-disabled"))) {
+  //       document.getElementById(type.toString()).setAttribute("class", "disable-hover off");
+  //       this.devList = this.items.filter(item => item.month.startsWith(displayDate.getMonth()));
+  //       this.events.publish('myEvent', this.devList);
+  //       console.log(this.devList);
+  //     }
+  //     else if(type === 2 && !(document.getElementById("1").className.includes("btn-disabled")) && !(document.getElementById(type.toString()).className.includes("btn-disabled"))) {
+  //       document.getElementById(type.toString()).setAttribute("class", "btn-disabled");
+  //       this.devList = this.devList.filter(item => !item.kind.startsWith('work'));
+  //       this.devList = this.devList.filter(item => !item.kind.startsWith('off'));
+  //       this.events.publish('myEvent', this.devList);
+  //       console.log(this.devList);
+  //     }
+
+
+
+  //} 
+  // else if (type === 3) {
+  //   if (document.getElementById(type.toString()).className.includes("btn-disabled")) {
+  //     document.getElementById(type.toString()).setAttribute("class", "disable-hover trip");
+  //   }
+  //   else {
+  //     document.getElementById(type.toString()).setAttribute("class", "btn-disabled");
+  //   }
+  // }
+  // }
+
+  filterType(type: number) {
+    let displayDate = new Date(this.year);
+    this.devList = this.items.filter(item => item.month.startsWith(displayDate.getMonth()));
+    switch (type) {
+      case 1:
+        this.filterWork = !this.filterWork;
+        break;
+      case 2:
+        this.filterOff = !this.filterOff;
+        break;
+      case 3:
+        this.filterTrip = !this.filterTrip;
+        break;
+      default:
+        break;
+    }
+
+    if (!this.filterWork) {
+      this.devList = this.devList.filter(item => !item.kind.startsWith('work'));
+    }
+    // this.events.publish('myEvent', this.devList);
+
+    if (!this.filterOff) {
+      this.devList = this.devList.filter(item => !item.kind.startsWith('off'));
+    }
+    // this.events.publish('myEvent', this.devList);
+
+    if (!this.filterTrip) {
+      this.devList = this.devList.filter(item => !item.kind.startsWith('trip'));
+    }
+    this.events.publish('myEvent', this.devList);
+
+
+  }
 }
 
 
